e32e5cbae1b66777ef4bd129fedd899982e36ee7
AshYoon <rlawodbs7691@naver.com>
2023-12-09 14:43:50 +0900

    하노이 탑

--- 0819/test.cpp
+++ 0819/test.cpp
@@ -456,6 +456,21 @@ bool Eratos ( int n , int a )
 //         return 0;
 // }
 
+
+//BOJ 11729 하노이 탑 이동순서 
+void func ( int a , int b , int n)
+{
+    if( n == 1 ) // base condition 
+    {
+        cout << a << " " << b << '\n';
+        return ;
+    }
+    func ( a , 6 -a -b , n-1); // 먼저 n -1 개를 a 와 b 가 아닌 기둥으로 옮기고 
+    cout << a << " " << b << '\n'; // 그 다음 a 기둥의 제일 마지막 원판을 b 기둥으로 옮긴뒤 
+    func( 6 -a - b , b , n-1); // a 와 b 가 아닌 기둥에 있는 n -1 개의 원판을 b 로 옮겨 준다 
+}
+
+
 int n , m;
 #define X first
 #define Y second
@@ -463,19 +478,31 @@ int n , m;
 int dx[4] = {-1 , 1 , 0,0 };
 int dy[4] = { 0 , 0 , 1, -1};
 
+void recursion(int n)
+{
+    if( n > 0 )
+    {
+        cout << n << '\n';
+        recursion( n -1 );
+    }
+    
+}
 
-
+int recursionPlus(int n)
+{
+    if(n==0) return 0;
+    return n + recursionPlus(n -1 );
+}
 
 int main()
 {   
         ios::sync_with_stdio(0);
         cin.tie(0);
-
-        cin >> n >> m;
-
-
-
-
+        int k;
+        cin >> k;
+        
+        func( 1 ,3 ,k);
+        cout << (1 << k) -1 << '\n';
         return 0;
 
 
